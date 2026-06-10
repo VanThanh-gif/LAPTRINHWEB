7a4aa1c2923f783c597f614bbe90d193e4afff78
giabao263 <baopg1093@ut.edu.vn>
2026-06-10 16:36:59 +0700

    Update delete_document.php

---
+++ User/backend/documents/delete_document.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+
+require_once '../../../config/database.php';
+
+$user_id = $_SESSION['user_id'];
+
+$id = $_GET['id'];
+
+$stmt =
+$conn->prepare(
+"SELECT *
+FROM documents
+WHERE document_id=?
+AND user_id=?"
+);
+
+$stmt->bind_param(
+"ii",
+$id,
+$user_id
+);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$doc = $result->fetch_assoc();
+
+if($doc)
+{
+    if(file_exists($doc['file_path']))
+    {
+        unlink($doc['file_path']);
+    }
+
+    $delete =
+    $conn->prepare(
+    "DELETE FROM documents
+     WHERE document_id=?"
+    );
+
+    $delete->bind_param("i",$id);
+
+    $delete->execute();
+}
+
+header(
+"Location: ../../frontend/documents/my_documents.php"
+);
