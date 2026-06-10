2d706a5f648f4f7f31b67af2017de7979638ec3f
giabao263 <baopg1093@ut.edu.vn>
2026-06-10 16:35:41 +0700

    Create document_detail.php to show document info
    Fetch document details from the database and display them.

---
+++ User/frontend/documents/document_detail.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once '../../../config/database.php';
+
+$id = $_GET['id'];
+
+$sql =
+"SELECT * FROM documents
+ WHERE document_id=?";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("i",$id);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$doc = $result->fetch_assoc();
+?>
+
+<h2>Chi tiết tài liệu</h2>
+
+<p>
+<b>Tiêu đề:</b>
+<?= $doc['title'] ?>
+</p>
+
+<p>
+<b>Mô tả:</b>
+<?= $doc['description'] ?>
+</p>
+
+<p>
+<b>Tên file:</b>
+<?= $doc['file_name'] ?>
+</p>
+
+<p>
+<b>Loại:</b>
+<?= $doc['file_type'] ?>
+</p>
+
+<p>
+<b>Kích thước:</b>
+<?= $doc['file_size'] ?>
+ bytes
+</p>
